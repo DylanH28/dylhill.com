7c36841688e6b1aa454dfa0decf0b8d1bb7eea41
Dylan <dylanhill1228@gmail.com>
2022-04-08 20:20:10 -0500

    Added custom cursor

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+let mouseCursor = document.querySelector('.cursor');
+let buttons = document.querySelectorAll('.buttons');
 
+window.addEventListener('mousemove', cursor);
+
+function cursor(e) {
+    mouseCursor.style.top = e.clientY + 'px';
+    mouseCursor.style.left = e.clientX + 'px';
+}
+
+buttons.forEach(link => {
+    link.addEventListener('mouseleave', () => {
+        mouseCursor.classList.remove('linkGrow');
+    });
+    link.addEventListener('mouseover', () => {
+        mouseCursor.classList.add('linkGrow');
+    });
+});
